2b7f3932ce4497bc78f49e69c459ddab552b25ce
Thomas Santosa <70022040+Thomassantosa@users.noreply.github.com>
2023-11-20 13:36:02 +0700

    update-addstory

--- app/src/main/java/com/example/storyappsubmission/view/addStory/AddStoryActivity.kt
+++ app/src/main/java/com/example/storyappsubmission/view/addStory/AddStoryActivity.kt
@@ -1,12 +1,173 @@
 package com.example.storyappsubmission.view.addStory
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
+import android.content.Intent.ACTION_GET_CONTENT
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
-import com.example.storyappsubmission.R
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import androidx.lifecycle.ViewModelProvider
+import com.example.storyappsubmission.databinding.ActivityAddStoryBinding
+import com.example.storyappsubmission.helper.ViewModelFactory
+import com.example.storyappsubmission.utils.createTempFile
+import com.example.storyappsubmission.utils.reduceFileImage
+import com.example.storyappsubmission.utils.uriToFile
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.File
 
 class AddStoryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddStoryBinding
+    private lateinit var addStoryViewModel: AddStoryViewModel
+    private var getFile: File? = null
+    private var token: String? = null
+
+    companion object {
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
+    }
+
+    private lateinit var currentPhotoPath: String
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == RESULT_OK) {
+            val myFile = File(currentPhotoPath)
+
+            myFile.let { file ->
+                getFile = file
+                binding.previewImageView.setImageBitmap(BitmapFactory.decodeFile(file.path))
+            }
+        }
+    }
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val selectedImg = result.data?.data as Uri
+            selectedImg.let { uri ->
+                val myFile = uriToFile(uri, this@AddStoryActivity)
+                getFile = myFile
+                binding.previewImageView.setImageURI(uri)
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_story)
+        binding = ActivityAddStoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (!allPermissionsGranted()) {
+            ActivityCompat.requestPermissions(
+                this,
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
+        binding.cameraButton.setOnClickListener { startTakePhoto() }
+        binding.galleryButton.setOnClickListener { startGallery() }
+        binding.uploadButton.setOnClickListener { uploadImage() }
+        binding.back.setOnClickListener{ finish() }
+
+        setupViewModel()
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun setupViewModel() {
+        addStoryViewModel = ViewModelProvider(
+            this,
+            ViewModelFactory.getInstance(this)
+        )[AddStoryViewModel::class.java]
+
+        addStoryViewModel.getUser().observe(this) { user ->
+            token = user.token
+        }
+
+        addStoryViewModel.isLoading.observe(this) {
+            showLoading(it)
+        }
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        if (isLoading) {
+            binding.progressBar.visibility = View.VISIBLE
+        } else {
+            binding.progressBar.visibility = View.GONE
+        }
+    }
+
+    private fun startTakePhoto() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.resolveActivity(packageManager)
+
+        createTempFile(application).also {
+            val photoURI: Uri = FileProvider.getUriForFile(
+                this@AddStoryActivity,
+                "com.example.storyappsubmission",
+                it
+            )
+            currentPhotoPath = it.absolutePath
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+            launcherIntentCamera.launch(intent)
+        }
+    }
+
+    private fun startGallery() {
+        val intent = Intent()
+        intent.action = ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Choose a Picture")
+        launcherIntentGallery.launch(chooser)
+    }
+
+    private fun uploadImage() {
+        if (getFile != null) {
+            val file = reduceFileImage(getFile as File)
+
+            val description = binding.descriptionEditText.text.toString().toRequestBody("text/plain".toMediaType())
+            val requestImageFile = file.asRequestBody("image/jpeg".toMediaType())
+            val imageMultipart: MultipartBody.Part = MultipartBody.Part.createFormData(
+                "photo",
+                file.name,
+                requestImageFile
+            )
+
+            addStoryViewModel.createStory(token!!, imageMultipart, description)
+
+            addStoryViewModel.addStoryResponse.observe(this) { response ->
+                Toast.makeText(this, response.message, Toast.LENGTH_SHORT).show()
+                if(response.error == false){
+                    finish()
+                }
+
+            }
+
+            addStoryViewModel.errorResponse.observe(this) { response ->
+                if(response.message != null){
+                    Toast.makeText(this, response.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        } else {
+            Toast.makeText(this@AddStoryActivity, "Please insert an image file first."
+                , Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/example/storyappsubmission/view/addStory/AddStoryViewModel.kt
+++ app/src/main/java/com/example/storyappsubmission/view/addStory/AddStoryViewModel.kt
@@ -1,6 +1,66 @@
 package com.example.storyappsubmission.view.addStory
 
+import android.content.ContentValues.TAG
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.storyappsubmission.data.api.ApiConfig
 import com.example.storyappsubmission.data.repository.Repository
+import com.example.storyappsubmission.data.responses.ErrorResponse
+import com.example.storyappsubmission.data.responses.LoginResult
+import com.google.gson.Gson
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class AddStoryViewModel(private val repository: Repository) {
+class AddStoryViewModel(private val repository: Repository) : ViewModel() {
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    private val _addStoryResponse = MutableLiveData<ErrorResponse>()
+    val addStoryResponse: LiveData<ErrorResponse> = _addStoryResponse
+
+    private val _errorResponse = MutableLiveData<ErrorResponse>()
+    val errorResponse: LiveData<ErrorResponse> = _errorResponse
+
+    fun createStory(token:String, imageMultipart: MultipartBody.Part, description : RequestBody){
+        _isLoading.value = true
+        val apiService = ApiConfig().getApiService()
+        val uploadImageRequest = apiService.uploadImage("Bearer $token", imageMultipart, description)
+
+        uploadImageRequest.enqueue(object : Callback<ErrorResponse> {
+            override fun onResponse(
+                call: Call<ErrorResponse>,
+                response: Response<ErrorResponse>
+            ) {
+                _isLoading.value = false
+                if (response.isSuccessful) {
+                    val responseBody = response.body()
+                    if (responseBody != null && !responseBody.error!!) {
+                        _addStoryResponse.value = response.body()
+                    }
+                } else {
+                    val errBody = response.errorBody()
+                    val errJsonString = errBody?.string()
+                    val gson = Gson()
+                    _errorResponse.value = gson.fromJson(errJsonString, ErrorResponse::class.java)
+                    Log.e(TAG, "onFailure: ${response.message()}")
+                }
+                _errorResponse.value = ErrorResponse(null, null)
+            }
+
+            override fun onFailure(call: Call<ErrorResponse>, t: Throwable) {
+                _isLoading.value = false
+                Log.e(TAG, "onFailure: ${t.message}")
+            }
+        })
+    }
+
+    fun getUser(): LiveData<LoginResult> {
+        return repository.getUser()
+    }
 }
